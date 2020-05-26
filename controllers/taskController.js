fdfddc496dd3c5330889aa34a27608947b4b9c92
Roy Park <roydepark97@gmail.com>
2020-05-26 16:05:05 +0900

    Add files via upload

---
+++ controllers/taskController.js
@@ -0,0 +1,39 @@
+var Task = require('../models/task');
+
+exports.task_list = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
+
+exports.task_detail = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_detail");
+};
+
+// GET create
+exports.task_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
+
+// POST create
+exports.task_create_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
+
+// GET delete
+exports.task_delete_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
+
+// POST delete
+exports.task_delete_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
+
+// GET update
+exports.task_update_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
+
+// POST update
+exports.task_update_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: task_list");
+};
